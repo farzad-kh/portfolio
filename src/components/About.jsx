2af46839d7671453c42aef2bf30432081c391e0a
farzad kh <farzadkh900@gmail.com>
2024-05-14 05:48:10 +0330

    fixing layout shifting

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import {AboutMe} from "../components/framer/Typing"
 import { motion } from 'framer-motion';
 const About = () => {
   return (
-    <motion.div viewport={{ once: true }} name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <motion.div viewport={{ once: true }} name='about' className='w-full h-screen bg-[#0a192f] text-gray-300 overflow-hidden'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1200px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
